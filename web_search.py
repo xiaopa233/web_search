167ce28716d6c5cc2ec4d6876e2ffb0dd63e7b73
xiaopa233 <xiaopa233@qq.com>
2024-10-11 04:21:58 +0800

    增加了移除链接的功能

--- web_search.py
+++ web_search.py
@@ -34,17 +34,23 @@ class HelpFunctions:
     def generate_excerpt(self, content, max_length=200):
         return content[:max_length] + "..." if len(content) > max_length else content
 
-    def format_text(self, original_text):
+    def format_text(self, original_text, valves):
         soup = BeautifulSoup(original_text, "html.parser")
         formatted_text = soup.get_text(separator=" ", strip=True)
         formatted_text = unicodedata.normalize("NFKC", formatted_text)
         formatted_text = re.sub(r"\s+", " ", formatted_text)
         formatted_text = formatted_text.strip()
         formatted_text = self.remove_emojis(formatted_text)
+        if valves.REMOVE_LINKS:
+            formatted_text = self.replace_urls_with_text(formatted_text)
         return formatted_text
 
     def remove_emojis(self, text):
         return "".join(c for c in text if not unicodedata.category(c).startswith("So"))
+
+    def replace_urls_with_text(self, text, replacement="(links)"):
+        pattern = r'\(https?://[^\s]+\)'
+        return re.sub(pattern, replacement, text)
 
     def process_search_result(self, result, valves):
         title_site = self.remove_emojis(result["title"])
@@ -68,7 +74,7 @@ class HelpFunctions:
             html_content = response_site.text
 
             soup = BeautifulSoup(html_content, "html.parser")
-            content_site = self.format_text(soup.get_text(separator=" ", strip=True))
+            content_site = self.format_text(soup.get_text(separator=" ", strip=True), valves)
 
             truncated_content = self.truncate_to_n_words(
                 content_site, valves.PAGE_CONTENT_WORDS_LIMIT
@@ -184,6 +190,10 @@ class Tools:
             default=False,
             description="是否启用RAG",
         )
+        REMOVE_LINKS: bool = Field(
+            default=True,
+            description="检索中的返回是否移除链接",
+        )
         OPEN_WEBUI_TOKEN: str = Field(
             default="",
             description="open-webui令牌",
@@ -294,7 +304,6 @@ class Tools:
                                 },
                             }
                         )
-            else:
                 if len(results_json):
                     await emitter.message(
                         "\n<details>\n<summary>检索到的网站标题</summary>\n"
